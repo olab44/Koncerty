979c5064911124634e829205cc3ec3bd4860e1f5
Krzysztof Golcz <krzysgolcz@gmail.com>
2024-12-12 14:41:15 +0100

    Updated db_init script to take input from commandline and removed not needed .pgpass

--- database/db_init.sh
+++ database/db_init.sh
@@ -1,7 +1,8 @@
-# db_pass=$1
-# export PGPASSWORD=db_pass
-# echo $PGPASSWORD
-chmod 0600 .pgpass
-cp .pgpass ~/
-psql -h localhost -U postgres -d koncerty_db -p 5432 < init_bd.sql 
-psql -h localhost -U postgres -d koncerty_db -p 5432 < populate.sql 
+db_port=$1
+db_name=$2
+db_user=$3
+db_pass=$4  
+# chmod 0600 .pgpass
+# cp .pgpass ~/
+PGPASSWORD=$db_pass psql -h localhost -U $db_user -d $db_name -p $db_port < init_bd.sql 
+PGPASSWORD=$db_pass psql -h localhost -U $db_user -d $db_name -p $db_port < populate.sql 
