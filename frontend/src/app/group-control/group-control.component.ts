571656ad5a7a718e404bf915d90e74a5047a2864
zjasina <Z.Jasina@o2.pl>
2024-12-18 06:44:13 +0100

    frontend: group control basic layout

--- frontend/src/app/group-control/group-control.component.ts
+++ frontend/src/app/group-control/group-control.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { TopBarComponent } from '../bars/top-bar.component';
+import { SideBarComponent } from '../bars/side-bar.component';
 
 @Component({
   selector: 'app-group-control',
   standalone: true,
-  imports: [TopBarComponent],
+  imports: [TranslateModule, TopBarComponent, SideBarComponent],
   templateUrl: './group-control.component.html',
   styleUrl: './group-control.component.css'
 })
