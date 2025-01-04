f4fd17a8e1ee5ce14df4b96137844d9b9f92c3ee
zjasina <Z.Jasina@o2.pl>
2025-01-04 13:27:14 +0100

    fix: sidebar button layout

--- frontend/src/app/bars/side-bar.component.ts
+++ frontend/src/app/bars/side-bar.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'
 import { TranslateModule } from '@ngx-translate/core';
 import { GroupInfo } from '../interfaces';
@@ -7,7 +6,7 @@ import { GroupInfo } from '../interfaces';
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [CommonModule, FormsModule, TranslateModule],
+  imports: [FormsModule, TranslateModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
