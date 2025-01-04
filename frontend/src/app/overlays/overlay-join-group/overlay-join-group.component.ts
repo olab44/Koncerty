4687982c1a52f6169c1659f1b6e879c9526a1395
zjasina <Z.Jasina@o2.pl>
2025-01-04 15:05:14 +0100

    frontend: join group

--- frontend/src/app/overlays/overlay-join-group/overlay-join-group.component.ts
+++ frontend/src/app/overlays/overlay-join-group/overlay-join-group.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
+import { BackendService } from '../../services/backend-connection/backend.service';
 
 @Component({
   selector: 'app-overlay-join-group',
@@ -10,18 +11,23 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrl: './overlay-join-group.component.css'
 })
 export class OverlayJoinGroupComponent {
+  constructor(private backend: BackendService) {}
+
   @Output() close = new EventEmitter<void>()
 
   joinMessage: string = '...'
   codeInput: string = ""
 
-  validateCode(groupCode: string) : boolean {
-    return groupCode !== ""
-  }
-
-  joinGroup(groupCode: string) : void {
-    // TODO: API CALL
-    this.joinMessage = this.validateCode(groupCode) ? "<<group joining status>>" : "Please, input the valid code."
+  joinGroup(inv_code: string) : void {
+    this.backend.postRequest('groups/joinGroup', { inv_code }).subscribe({
+      next: res => {
+        this.joinMessage = `Group joined successfully.`
+      },
+      error: e => {
+        console.log(e)
+        this.joinMessage = e.detail || "Unexpected error occured."
+      }
+    })
   }
 
   closeOverlay() {
