e1b79be1b3ff2031adedad1c5b248bdaac5b889e
bilb <krzysgolcz@gmail.com>
2025-01-06 00:00:18 +0100

    Add working /events/findEvents

--- backend/src/events/service.py
+++ backend/src/events/service.py
@@ -3,12 +3,14 @@ from datetime import datetime
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
 from dotenv import load_dotenv
+from typing import List
 
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 
-from .schemas import EventInfo, CreateEventRequest
-from users.models import User
+from .schemas import EventInfo, CreateEventRequest, GetEventInfo, Participant, CompositionInfo
+from users.models import User, Member
+from users.models import Event, Participation, Composition
 
 def get_calendar_service(token: str):
     try:
@@ -22,15 +24,107 @@ def get_calendar_service(token: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Google Calendar service error: {str(e)}")
 
-def get_user_events(db: Session, email: str):
-    event = EventInfo(event_id = 1, name = 'Code Review',
-        description ='Review latest PRs',
-        date_start = datetime(2025, 1, 11, 14, 0, 0),
-        date_end = datetime(2025, 1, 11, 15, 30, 0),
-        set_list = [],
-        participants = [])
-    return [event]
+def get_setlist_info(db: Session, setlists: List[int]):
+    setlist_infos = []
+    for setlist in setlists:
+        composition = db.query(Composition).filter(Composition.id == setlist.id).first()
+        setlist_infos.append(
+            CompositionInfo(id=composition.id, name=composition.name, author=composition.author)
+        )
+    return setlist_infos
 
-def create_event(db: Session, event: CreateEventRequest):
+def get_participants_info(db: Session, participants: List[int]):
+    user_infos = []
+    for participant in participants:
+        user = db.query(User).filter(User.id == participant.id).first()
+        user_infos.append(
+            Participant(id=user.id, username=user.username, email=user.email)
+        )
+    return user_infos
 
-    return
+def get_user_events(db: Session, email: str, request: GetEventInfo):
+    existing_user = db.query(User).filter(User.email == email).first()
+    if not existing_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    group_events = (
+        db.query(Event)
+        .join(Participation, Participation.event_id == Event.id)
+        .filter(
+            Participation.user_id == existing_user.id, 
+            Event.parent_group == request.group_id  
+        )
+        .all()
+    )
+    event_infos = []
+    for event in group_events:
+        event_infos.append(EventInfo(
+            event_id = event.id,
+            name = event.name,
+            date_start = event.date_end,
+            date_end = event.date_end,
+            location = event.location,
+            extra_info = event.extra_info,
+            set_list = get_setlist_info(db, event.set_lists),
+            parent_group = event.parent_group,
+            type = event.type,
+            participants = get_participants_info(db, event.participations))
+        )
+
+    return event_infos
+
+def create_event(db: Session, email: str, request: CreateEventRequest):
+    
+    existing_user = db.query(User).filter(User.email == email).first()
+    if not existing_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    new_event = Event(
+        id=request.id,
+        name = request.name,
+        date_start = request.date_start,
+        date_end = request.date_end,
+        location = request.location,
+        extra_info = request.extra_info,
+        parent_group = request.parent_group,
+        type = request.type
+    )
+
+    db.add(new_event)
+    db.commit()
+    db.refresh(new_event)
+
+    new_participation = Participation(
+        user_id=existing_user.id,
+        event_id=new_event.id,
+    )
+    db.add(new_participation)
+
+    unique_user_ids = set(request.user_ids)
+
+    for user_email in request.user_emails:
+        user = db.query(User).filter(User.email == user_email).first()
+        unique_user_ids.add(user.id)
+
+    for group_id in request.group_ids:
+        members = db.query(Member).filter(Member.group_id == group_id).all()
+        for member in members:
+            unique_user_ids.add(member.user_id)
+
+    for user_id in unique_user_ids:
+        existing_participation = db.query(Participation).filter(
+            Participation.event_id == new_event.id,
+            Participation.user_id == user_id
+        ).first()
+
+        if not existing_participation:
+            new_participation = Participation(
+                user_id=user_id,
+                event_id=new_event.id
+            )
+            db.add(new_participation)
+
+    db.commit()
+
+    
+    return new_event
--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column, BigInteger, String, ForeignKey
+from sqlalchemy import Column, BigInteger, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from database import Base
-
 
 class User(Base):
     __tablename__ = "users"
@@ -20,8 +19,13 @@ class Group(Base):
     name = Column(String(30), nullable=False)
     extra_info = Column(String(100))
     invitation_code = Column(String(20), nullable=False)
-    members = relationship("Member", back_populates="group")
 
+
+    members = relationship("Member", back_populates="group")
+    events = relationship(
+        "Event",
+        back_populates="group",
+        primaryjoin="Group.id == Event.parent_group" )
 
 class Member(Base):
     __tablename__ = "members"
@@ -33,3 +37,54 @@ class Member(Base):
 
     user = relationship("User", back_populates="members")
     group = relationship("Group", back_populates="members")
+
+
+
+class Event(Base):
+    __tablename__ = 'events'
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)  
+    date_start = Column(DateTime, nullable=False)
+    date_end = Column(DateTime, nullable=False)
+    location = Column(String(100), nullable=False)  
+    extra_info = Column(String(100), nullable=True)  
+    parent_group = Column(BigInteger, ForeignKey('groups.id'), nullable=False)  
+    type = Column(String(8), nullable=False)
+
+    participations = relationship("Participation", back_populates="event")
+    set_lists = relationship("SetList", back_populates="event")
+    
+    group = relationship("Group", back_populates="events")
+
+
+class Participation(Base):
+    __tablename__ = 'participations'
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    event_id = Column(BigInteger, ForeignKey('events.id'), nullable=False)  
+    user_id = Column(BigInteger, ForeignKey('users.id'), nullable=False)  
+
+    event = relationship("Event", back_populates="participations")
+
+
+class SetList(Base):
+    __tablename__ = 'set_lists'
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    event_id = Column(BigInteger, ForeignKey('events.id'), nullable=False)
+    composition_id = Column(BigInteger, ForeignKey('compositions.id'), nullable=False)
+
+    event = relationship("Event", back_populates="set_lists")
+    composition = relationship("Composition", back_populates="set_lists")
+
+
+class Composition(Base):
+    __tablename__ = 'compositions'
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    author = Column(String(40), nullable=True)
+
+    set_lists = relationship("SetList", back_populates="composition")
+
