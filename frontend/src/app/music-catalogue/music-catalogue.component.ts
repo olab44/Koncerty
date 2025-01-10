b83f836524686c77791a85b1f81fc62bc53b148c
zjasina <Z.Jasina@o2.pl>
2025-01-10 01:58:30 +0100

    music catalogue - title searching

--- frontend/src/app/music-catalogue/music-catalogue.component.ts
+++ frontend/src/app/music-catalogue/music-catalogue.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { TopBarComponent } from '../bars/top-bar/top-bar.component';
 import { OverlayAddCompositionComponent } from '../overlays/overlay-add-composition/overlay-add-composition.component';
 import { GroupInfo } from '../interfaces';
 import { SessionStateService } from '../services/session-state/session-state.service';
+import { FilterPipe } from '../pipe/filter.pipe';
 
 @Component({
   selector: 'app-music-catalogue',
   standalone: true,
-  imports: [ CommonModule, TranslateModule, TopBarComponent, OverlayAddCompositionComponent ],
+  imports: [ CommonModule, FormsModule, TranslateModule, TopBarComponent, OverlayAddCompositionComponent, FilterPipe ],
   templateUrl: './music-catalogue.component.html',
   styleUrl: './music-catalogue.component.css'
 })
@@ -17,6 +19,7 @@ export class MusicCatalogueComponent {
   group!: GroupInfo
   viewedComposition = {title: "", author: ""}
   visibleOverlayComposition = false
+  searchPhrase: string = ""
 
   compositions = [
     {title: "We'll be fine", author: "Jorge Rivera-Herrans"}
